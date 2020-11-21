1c9c196ba233db55347c98b4c8d2d42c67ac2888
Mariya Burdo <62555174+mashaBurdo@users.noreply.github.com>
2020-11-21 15:06:07 +0300

    ready for check

--- Ex1/main.py
+++ Ex1/main.py
@@ -10,19 +10,21 @@ def createParser():
 
     return parser
 
-parser = createParser()
-namespace = parser.parse_args(sys.argv[1:])
 
-students = LoadJson(namespace.students).loadfile()
-rooms = LoadJson(namespace.rooms).loadfile()
+if __name__ == "__main__":
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
 
-rooms_students = RoomsForStudents(rooms, students).create_list()
+    students = LoadJson(namespace.students).loadfile()
+    rooms = LoadJson(namespace.rooms).loadfile()
 
-if namespace.format == 'json':
-    UploadJson(rooms_students).create_file('rooms_students.json')
-elif namespace.format == 'xml':
-    UploadXml(rooms_students).create_file('rooms_students.xml')
-else:
-    print('Wrong format!')
+    rooms_students = RoomsForStudents(rooms, students).create_list()
+
+    if namespace.format == 'json':
+        UploadJson(rooms_students).create_file('rooms_students.json')
+    elif namespace.format == 'xml':
+        UploadXml(rooms_students).create_file('rooms_students.xml')
+    else:
+        print('Wrong format!')
 
 # Command example: python main.py -s students.json -r rooms.json -f json      
