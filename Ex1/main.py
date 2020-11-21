173769cd533241402c72a66535886c50c43b218c
Mariya Burdo <62555174+mashaBurdo@users.noreply.github.com>
2020-11-21 14:55:52 +0300

    ready for check

--- Ex1/main.py
+++ Ex1/main.py
@@ -1,9 +1,28 @@
 from modules import LoadJson, RoomsForStudents, UploadJson, UploadXml
+import sys
+import argparse
 
-rooms = LoadJson('rooms.json').loadfile()
-students = LoadJson('students.json').loadfile()
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--students')
+    parser.add_argument('-r', '--rooms')
+    parser.add_argument('-f', '--format')
+
+    return parser
+
+parser = createParser()
+namespace = parser.parse_args(sys.argv[1:])
+
+students = LoadJson(namespace.students).loadfile()
+rooms = LoadJson(namespace.rooms).loadfile()
 
 rooms_students = RoomsForStudents(rooms, students).create_list()
 
-UploadJson(rooms_students).create_file('rooms_students.json')
-UploadXml(rooms_students).create_file('rooms_students.xml')
+if namespace.format == 'json':
+    UploadJson(rooms_students).create_file('rooms_students.json')
+elif namespace.format == 'xml':
+    UploadXml(rooms_students).create_file('rooms_students.xml')
+else:
+    print('Wrong format!')
+
+# Command example: python main.py -s students.json -r rooms.json -f json      
