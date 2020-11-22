06573cad0cbfa025ab3c19cf66d75dd03a68f3c6
Mariya Burdo <62555174+mashaBurdo@users.noreply.github.com>
2020-11-22 17:22:12 +0300

    ready for check

--- Ex2/task_2.py
+++ Ex2/task_2.py
@@ -1,10 +1,18 @@
 class Version:
+    '''Class for lexicographical version comparison. Versions are represented as tuples. Leading zeros problem is solved.'''
     def __init__(self, version):
         self.version = self.to_tuple(version)
     
     def to_tuple(self, version):
-        version = tuple(version.split('.'))
-        return version
+        version = version.split('.')
+        new_version = []
+        for i in version:
+            try:
+                i = int(i)
+                new_version.append(i)
+            except ValueError:
+                new_version.append(i)
+        return tuple(new_version)
 
     def _cmp(self, other):
         if self.version == other.version:
@@ -14,10 +22,6 @@ class Version:
         if self.version > other.version:
             return 1
 
-    def __lt__(self, other):
-        c = self._cmp(other)
-        return c < 0
-    
     def __gt__(self, other):
         c = self._cmp(other)
         return c > 0
@@ -25,10 +29,6 @@ class Version:
     def __eq__(self, other):
         c = self._cmp(other)
         return c == 0
-    
-    def __le__(self, other):
-        c = self._cmp(other)
-        return c <= 0
     
     def __ge__(self, other):
         c = self._cmp(other)
@@ -42,20 +42,32 @@ def main():
         ("1.2.0", "1.2.42"),
         ("1.1.0-alpha", "1.2.0-alpha.1"),
         ("1.0.1b", "1.0.10-alpha.beta"), 
-        ("1.0.0-rc.1", "1.0.0"), 
-        #("1.0.10-alpha.beta", "1.0.1b"),
-        #("1.0.0", "1.0.0-rc.1")
+        ("1.0.0-rc.1", "1.0.0")
 
     ]
 
     for version_1, version_2 in to_test:
         
-        print(f"{version_1} and {version_2}", "<")
-        print(Version(version_1) < Version(version_2))
-        #assert Version(version_1) < Version(version_2), "le failed" 
-        #assert Version(version_2) > Version(version_1), "ge failed"
-        #assert Version(version_2) != Version(version_1), "neq failed"
-'''
+        #print(Version(version_1).version, Version(version_2).version)
+        #print(f"{version_1} and {version_2}", "<")
+        #print(Version(version_1) < Version(version_2))
+        assert Version(version_1) < Version(version_2), "le failed" 
+        assert Version(version_2) > Version(version_1), "ge failed"
+        assert Version(version_2) != Version(version_1), "neq failed"
+
 if __name__ == "__main__":
     main()
+
+''' Command examples:
+>>> from task_2 import Version
+>>> Version('1.1.3') < Version('2.2.3')
+True
+>>> Version('1.3.0') > Version('0.3.0')
+True
+>>> Version('0.3.0b') < Version('1.2.42')
+True
+>>> Version('1.3.42') == Version('42.3.1')  
+False
+>>> Version('1.3.42') >= Version('00042.3.1')
+False
 '''
