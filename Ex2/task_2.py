b57a419e5aa13123b74a25efb7dbe29ccd67df88
Mariya Burdo <62555174+mashaBurdo@users.noreply.github.com>
2020-11-22 17:25:10 +0300

    ready for check

--- Ex2/task_2.py
+++ Ex2/task_2.py
@@ -2,7 +2,7 @@ class Version:
     '''Class for lexicographical version comparison. Versions are represented as tuples. Leading zeros problem is solved.'''
     def __init__(self, version):
         self.version = self.to_tuple(version)
-    
+
     def to_tuple(self, version):
         version = version.split('.')
         new_version = []
@@ -29,7 +29,7 @@ class Version:
     def __eq__(self, other):
         c = self._cmp(other)
         return c == 0
-    
+
     def __ge__(self, other):
         c = self._cmp(other)
         return c >= 0
@@ -41,19 +41,20 @@ def main():
         ("1.0.0", "1.42.0"),
         ("1.2.0", "1.2.42"),
         ("1.1.0-alpha", "1.2.0-alpha.1"),
-        ("1.0.1b", "1.0.10-alpha.beta"), 
+        ("1.0.1b", "1.0.10-alpha.beta"),
         ("1.0.0-rc.1", "1.0.0")
 
     ]
 
     for version_1, version_2 in to_test:
-        
-        #print(Version(version_1).version, Version(version_2).version)
-        #print(f"{version_1} and {version_2}", "<")
-        #print(Version(version_1) < Version(version_2))
-        assert Version(version_1) < Version(version_2), "le failed" 
+
+        # print(Version(version_1).version, Version(version_2).version)
+        # print(f"{version_1} and {version_2}", "<")
+        # print(Version(version_1) < Version(version_2))
+        assert Version(version_1) < Version(version_2), "le failed"
         assert Version(version_2) > Version(version_1), "ge failed"
         assert Version(version_2) != Version(version_1), "neq failed"
+
 
 if __name__ == "__main__":
     main()
@@ -66,7 +67,7 @@ True
 True
 >>> Version('0.3.0b') < Version('1.2.42')
 True
->>> Version('1.3.42') == Version('42.3.1')  
+>>> Version('1.3.42') == Version('42.3.1')
 False
 >>> Version('1.3.42') >= Version('00042.3.1')
 False
