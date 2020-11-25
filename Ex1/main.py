be186858512c74dfc8c005e68acd2959c444b968
Mariya Burdo <62555174+mashaBurdo@users.noreply.github.com>
2020-11-25 16:28:06 +0300

    fixed 2 issues

--- Ex1/main.py
+++ Ex1/main.py
@@ -1,18 +1,32 @@
 from modules import LoadJson, RoomsForStudents, UploadJson, UploadXml
 import sys
 import argparse
+import os.path
 
-def createParser():
+def file_choice(extention, fname):
+    ext = os.path.splitext(fname)[1][1:]
+    if ext != extention:
+       parser.error("File extention not {}".format(extention))
+    return fname
+
+def extention_choice(ext):
+    if ext != 'xml' and ext != 'json':
+         parser.error("File extention not xml or json")
+    return ext
+
+def create_parser(): 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--students')
-    parser.add_argument('-r', '--rooms')
-    parser.add_argument('-f', '--format')
+    parser.add_argument('-s', '--students', required=True, type=lambda s:file_choice(('json'),s))
+    parser.add_argument('-r', '--rooms', required=True, type=lambda s:file_choice(('json'),s))
+    parser.add_argument('-f', '--format', required=True, type=lambda s:extention_choice(s))
 
     return parser
 
 
+
+
 if __name__ == "__main__":
-    parser = createParser()
+    parser = create_parser()
     namespace = parser.parse_args(sys.argv[1:])
 
     students = LoadJson(namespace.students).loadfile()
@@ -24,7 +38,6 @@ if __name__ == "__main__":
         UploadJson(rooms_students).create_file('rooms_students.json')
     elif namespace.format == 'xml':
         UploadXml(rooms_students).create_file('rooms_students.xml')
-    else:
-        print('Wrong format!')
+
 
 # Command example: python main.py -s students.json -r rooms.json -f json      
