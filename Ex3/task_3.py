5e14d0b76149a7bc6892064df8d43a0ad7047a33
Mariya Burdo <62555174+mashaBurdo@users.noreply.github.com>
2020-11-26 11:49:35 +0300

    conext manage

--- Ex3/task_3.py
+++ Ex3/task_3.py
@@ -1,19 +1,19 @@
 # Race condition is fixed
 from threading import Thread, Lock
+from multiprocessing.dummy import Pool as ThreadPool 
 
 a = 0
 lock = Lock()
 def function(arg):
-    global a
+    global a # Please don't use global variables
     for _ in range(arg):
-        lock.acquire()
-        a += 1
-        lock.release()
+        with lock: 
+            a += 1
 
 def main():
     threads = []
     for i in range(5):
-        thread = Thread(target=function, args=(1000000,))
+        thread = Thread(target=function, args=(100,)) #Please use thread pool 1000000
         thread.start()
         threads.append(thread)
 
